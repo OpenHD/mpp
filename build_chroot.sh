918542a6cd4a72570d5830d872cb52f2cd70a0ce
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-02-01 18:08:04 +0100

    Update build_chroot.sh

--- build_chroot.sh
+++ build_chroot.sh
@@ -12,34 +12,34 @@ cd build/linux/aarch64
 make -j$(nproc)
 make DESTDIR=mpp-package -j4 install
 echo "Current directory: $(pwd)"
-cd mpp-package/usr/local
-mv lib ../
-cd ../
-mkdir -p aarch64-linux-gnu
-cd lib
-mv * ../aarch64-linux-gnu/
-mv ../aarch64-linux-gnu ../lib/aarch64-linux-gnu
-cd ../../../
-fpm -a arm64 -s dir -t deb -n mpp -v 1.0 -C mpp-package -p mpp_VERSION_ARCH.deb
-echo "copied deb file"
-echo "push to cloudsmith"
-git describe --exact-match HEAD >/dev/null 2>&1
-echo "Pushing the package to OpenHD 2.3 repository"
-ls -a
-ls -a ../../../
-API_KEY=$(cat ../../../cloudsmith_api_key.txt)
-DISTRO=$(cat ../../../distro.txt)
-FLAVOR=$(cat ../../../flavor.txt)
-BOARD=$(cat ../../../board.txt)
+# cd mpp-package/usr/local
+# mv lib ../
+# cd ../
+# mkdir -p aarch64-linux-gnu
+# cd lib
+# mv * ../aarch64-linux-gnu/
+# mv ../aarch64-linux-gnu ../lib/aarch64-linux-gnu
+# cd ../../../
+# fpm -a arm64 -s dir -t deb -n mpp -v 1.0 -C mpp-package -p mpp_VERSION_ARCH.deb
+# echo "copied deb file"
+# echo "push to cloudsmith"
+# git describe --exact-match HEAD >/dev/null 2>&1
+# echo "Pushing the package to OpenHD 2.3 repository"
+# ls -a
+# ls -a ../../../
+# API_KEY=$(cat ../../../cloudsmith_api_key.txt)
+# DISTRO=$(cat ../../../distro.txt)
+# FLAVOR=$(cat ../../../flavor.txt)
+# BOARD=$(cat ../../../board.txt)
 
-if [ "$BOARD" = "rk3566" ]; then
-    for file in *.deb; do
-        mv "$file" "${file%.deb}-rk3566.deb"
-    done
-    cloudsmith push deb --api-key "$API_KEY" openhd/dev-release/${DISTRO}/${FLAVOR} *.deb || exit 1
-else
-for file in *.deb; do
-        mv "$file" "${file%.deb}-rk3588.deb"
-    done
-    cloudsmith push deb --api-key "$API_KEY" openhd/dev-release/${DISTRO}/${FLAVOR} *.deb || exit 1
-fi
+# if [ "$BOARD" = "rk3566" ]; then
+#     for file in *.deb; do
+#         mv "$file" "${file%.deb}-rk3566.deb"
+#     done
+#     cloudsmith push deb --api-key "$API_KEY" openhd/dev-release/${DISTRO}/${FLAVOR} *.deb || exit 1
+# else
+# for file in *.deb; do
+#         mv "$file" "${file%.deb}-rk3588.deb"
+#     done
+#     cloudsmith push deb --api-key "$API_KEY" openhd/dev-release/${DISTRO}/${FLAVOR} *.deb || exit 1
+# fi
