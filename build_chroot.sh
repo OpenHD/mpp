417300c7ab431a5485e9b3dba1ab72bf466b6f2b
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-02-01 22:38:04 +0100

    build in ci

--- build_chroot.sh
+++ build_chroot.sh
@@ -6,9 +6,6 @@ sudo apt install -y python3-pip
 sudo pip3 install --upgrade cloudsmith-cli
 sudo apt install -y git ruby-dev make cmake gcc g++ wget libdrm-dev mlocate 
 gem install fpm
-sudo updatedb && locate -e drm.h
-ls -a /usr/include/
-exit 1
 cd build/linux/aarch64
 ./make-Makefiles.bash
 make -j$(nproc)
