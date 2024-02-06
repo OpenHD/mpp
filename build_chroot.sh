a8eb23bf8a5a32f7b1c963588ee9779abfdecb4f
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-02-06 16:44:41 +0100

    Update build_chroot.sh

--- build_chroot.sh
+++ build_chroot.sh
@@ -5,11 +5,8 @@
 sudo apt install -y python3-pip
 sudo pip3 install --upgrade cloudsmith-cli
 sudo apt install -y git ruby-dev make cmake gcc g++ wget libdrm-dev mlocate 
-apt install -y libdrm*
-sudo updatedb && locate -e drm.h
 gem install fpm
-cd build/linux/aarch64
-ldconfig
+ls -a \usr\lib\aarch64-linux-gnu\ |grep libdrm
 ./make-Makefiles.bash
 make -j$(nproc)
 make DESTDIR=mpp-package -j4 install
